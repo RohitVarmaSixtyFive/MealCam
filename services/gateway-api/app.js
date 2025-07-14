c1c6588e716c8215b9c7efd1b4cc69b6ee1fa595
RohitVarmaSixtyFive <rohit.chiluvuri@students.iiit.ac.in>
2025-07-14 12:35:39 +0530

    progress before nginx

--- services/gateway-api/app.js
+++ services/gateway-api/app.js
@@ -79,6 +79,8 @@ app.use('*', (req, res) => {
 
 const PORT = process.env.GATEWAY_PORT || 4000;
 
+app.set('trust proxy', true);
+
 app.listen(PORT, () => {
   console.log(`API Gateway running on port ${PORT}`);
   console.log('Service URLs:');
